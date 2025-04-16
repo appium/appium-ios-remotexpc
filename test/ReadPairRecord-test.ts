e2d84e1081664bd474c4324d35abb6cd0d139f4c
Sai Krishna V <saikrishna321@yahoo.com>
2025-04-16 19:03:37 +0530

    Added new service to get wifi, battery and power
    Co-authored-by: SrinivasanTarget <srinivasan.sekar1990@gmail.com>

--- test/ReadPairRecord-test.ts
+++ test/ReadPairRecord-test.ts
@@ -1,19 +1,16 @@
 import { createUsbmux } from '../src/lib/usbmux/index.js';
 
 async function test() {
-
-    try {
-        const usb = await createUsbmux();
-        await usb.readPairRecord('00008110-001854423C3A801E');
-        console.log(await usb.listDevices());
-        await usb.close();
-
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const usb = await createUsbmux();
+    await usb.readPairRecord('00008130000474C114E1001C');
+    console.log(await usb.listDevices());
+    await usb.close();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
-test().catch((err)=> {
-    console.log(err);
+test().catch((err) => {
+  console.log(err);
 });
