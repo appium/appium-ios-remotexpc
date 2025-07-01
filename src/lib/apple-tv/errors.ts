2b413acd44fcc34bf0863db227566bde1aeb88dd
Sri Harsha <sri_harsha509@hotmail.com>
2025-07-01 13:44:37 -0400

    refactor: simplify error classes and fix prototype chain handling in AppleTVError

--- src/lib/apple-tv/errors.ts
+++ src/lib/apple-tv/errors.ts
@@ -2,6 +2,8 @@
 export class AppleTVError extends Error {
   constructor(message: string) {
     super(message);
+    this.name = new.target.name;
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
 
@@ -17,29 +19,13 @@ export class PairingError extends AppleTVError {
 }
 
 // Represents an error related to network communication
-export class NetworkError extends AppleTVError {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class NetworkError extends AppleTVError {}
 
 // Represents an error occurring during cryptographic operations
-export class CryptographyError extends AppleTVError {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class CryptographyError extends AppleTVError {}
 
 // Represents an error specific to SRP (Secure Remote Password) protocol
-export class SRPError extends AppleTVError {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class SRPError extends AppleTVError {}
 
 // Represents an error related to TLV8 encoding/decoding
-export class TLV8Error extends AppleTVError {
-  constructor(message: string) {
-    super(message);
-  }
-}
+export class TLV8Error extends AppleTVError {}
