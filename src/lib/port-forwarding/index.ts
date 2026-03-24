8c2cca482a1d53ca9ecd3cfc4a9c7bb7a956d997
Mykola Mokhnach <mokhnach@gmail.com>
2026-03-24 08:50:13 +0100

    moar

--- src/lib/port-forwarding/index.ts
+++ src/lib/port-forwarding/index.ts
@@ -2,7 +2,6 @@ export { connectViaTunnel, connectViaUsbmux } from './connectors.js';
 export type {
   DevicePortForwarderEvents,
   DevicePortForwarderOptions,
-  PortForwardingConnector,
   UpstreamSocketConnector,
 } from './types.js';
 export { DevicePortForwarder } from './device-port-forwarder.js';
