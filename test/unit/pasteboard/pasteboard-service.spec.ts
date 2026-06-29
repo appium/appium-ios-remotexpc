2b5d78a04ebfd526aba4e9beaa6f67bd00d41d30
Mykola Mokhnach <mokhnach@gmail.com>
2026-06-29 16:59:04 +0200

    format

--- test/unit/pasteboard/pasteboard-service.spec.ts
+++ test/unit/pasteboard/pasteboard-service.spec.ts
@@ -111,7 +111,9 @@ describe('PasteboardService', function () {
       }));
       const service = new TestPasteboardService(fake);
 
-      expect((await service.getUrl())?.toString()).to.equal('https://example.test/path');
+      expect((await service.getUrl())?.toString()).to.equal(
+        'https://example.test/path',
+      );
     });
 
     it('setImage sends SET with a PNG payload', async function () {
